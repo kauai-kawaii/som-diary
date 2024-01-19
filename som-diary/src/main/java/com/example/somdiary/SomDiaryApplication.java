ecb5a042469604bab5eeaac15165dcdb48b6c1a9
fbgjung <fbgjung@gmail.com>
2024-01-19 15:19:40 +0900

    create project

---
+++ som-diary/src/main/java/com/example/somdiary/SomDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.somdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomDiaryApplication.class, args);
+	}
+
+}
---
+++ som-diary/src/test/java/com/example/somdiary/SomDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
