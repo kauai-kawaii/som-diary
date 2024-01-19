cce3b76483a684096ebf1745c8246d50c1c81d98
heogeonho <geonh1016@gmail.com>
2024-01-19 16:24:19 +0900

    calendar ui, react code upload
    start page, sign in page, calendar page

--- som-diary/src/main/views/src/App.js
+++ som-diary/src/main/views/src/App.js
@@ -1,10 +1,18 @@
-import Logo from "./components/Logo";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Start from './pages/Start';
+import Main from './pages/Main';
+
 function App() {
-  return (
-    <div className="App">
-      <Logo/>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' exact element={<Home />} />
+                <Route path='/get-start' exact element={<Start />} />
+                <Route path='/main' exact element={<Main />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
