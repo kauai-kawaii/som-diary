b1ddf86e8ecfa2935597ab1b49a7a74ba0a56633
heogeonho <geonh1016@gmail.com>
2024-03-07 22:37:25 +0900

    feat: 구글 응답을 받기 위한 dto 생성
    OAuth2Response -> 인터페이스 생성
    GoogleResponse -> 구글의 응답을 위한 바구니 dto 작성

---
+++ som-diary/src/main/java/com/example/somdiary/dto/GoogleResponse.java
@@ -0,0 +1,35 @@
+package com.example.somdiary.dto;
+
+import java.util.Map;
+
+public class GoogleResponse implements OAuth2Response{
+
+    private final Map<String, Object> attribute;
+
+    public GoogleResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+
+        return attribute.get("sub").toString(); // 구글의 경우 키: sub
+    }
+
+    @Override
+    public String getEmail() {
+
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+
+        return attribute.get("name").toString();
+    }
+}
