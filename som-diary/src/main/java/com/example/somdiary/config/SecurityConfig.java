055aee9fbfe6195d513f306542d51b9d68d4afbc
heogeonho <geonh1016@gmail.com>
2024-03-27 14:36:22 +0900

    chore: 시큐리티 설정, jwt 설정

--- som-diary/src/main/java/com/example/somdiary/config/SecurityConfig.java
+++ som-diary/src/main/java/com/example/somdiary/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.somdiary.config;
 
+import com.example.somdiary.jwt.JWTFilter;
+import com.example.somdiary.jwt.JWTUtil;
+import com.example.somdiary.oauth2.CustomSuccessHandler;
 import com.example.somdiary.service.CustomOAuth2UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -10,7 +13,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 @Configuration
@@ -18,10 +23,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler,  JWTUtil jwtUtil) {
 
         this.customOAuth2UserService = customOAuth2UserService;
+        // 핸들러 주입 (JWT 동작을 위함)
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
     }
 
 
@@ -38,36 +48,27 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
-//        http
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-//                        .requestMatchers("/").permitAll()
-//                        .requestMatchers("/get-start").permitAll()
-//                        .anyRequest().authenticated());
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
 
 
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/get-start/**").permitAll()
-                        .requestMatchers("/main/**").authenticated());
-
-
-//        http
-//                .oauth2Login(Customizer.withDefaults());
-
+                        .requestMatchers("/main/**", "/diary/**", "/search-location").authenticated()
+                        .anyRequest().permitAll());
 
         http
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService)));
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                );
 
-//        http
-//                .sessionManagement((session) -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build(); // 객체를 빌더타입으로 리턴
     }
-
 }
