43c46d99125ee700e615b4126294a0afcbc60903
heogeonho <geonh1016@gmail.com>
2024-04-05 20:17:04 +0900

    feat: JpaRepository로 수정

--- som-diary/src/main/java/com/example/somdiary/repository/UserRepository.java
+++ som-diary/src/main/java/com/example/somdiary/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.somdiary.repository;
 
 import com.example.somdiary.entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User,String> {
+public interface UserRepository extends JpaRepository<User,String> {
 
     User findByUserName(String username);
 }
